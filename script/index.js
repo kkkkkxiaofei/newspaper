6ab7dd9cd2251ada4694d3ea6b62369a6fd195bb
kkkkkxiaofei <brztzxf@gmail.com>
2015-10-27 22:14:28 +0800

    [XF] add weather forecast

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 (function() {
 	var viewModel = {
-		XLWB: ko.observableArray()
+		XLWB: ko.observableArray(),
+		XL_WEATHER: ko.observable()
 	}
 	var map = {
 		'XLWB': { 
@@ -15,6 +16,23 @@
 						}));
 					}
 				},
+		'XL_WEATHER': { 
+			url: 'http://open.weather.sina.com.cn/api/weather/sinaForecast?length=1&air=1&city=%E8%A5%BF%E5%AE%89&callback=?',
+			callback: function(response) {
+				console.log(response);
+				viewModel.XL_WEATHER({
+					location: response.result.data.info.name,
+					dayWeatherTemperature: response.result.data.days.day[0].day_temperature,
+					dayWeatherType: response.result.data.days.day[0].day_weather_type,
+					dayWindPower: response.result.data.days.day[0].day_wind_power,
+					dayWindType: response.result.data.days.day[0].day_wind_type,
+					nightWeatherTemperature: response.result.data.days.day[0].night_temperature,
+					nightWeatherType: response.result.data.days.day[0].night_weather_type,
+					nightWindPower: response.result.data.days.day[0].night_wind_power,
+					nightWindType: response.result.data.days.day[0].night_wind_type
+				});
+			}
+		},
 		'JRTT': 'http://i.snssdk.com/2/article/hot_comments/?callback=jQuery11110026725587202236056_1445440133029&_=1445440133030',
 		'JRTT1': 'http://i.snssdk.com/2/article/hot_comments/?callback=?',
 
@@ -37,7 +55,7 @@
 		});
 	}
 
-	_callJSONP(map['XLWB']);
+	_callJSONP(map['XL_WEATHER']);
 
 	ko.applyBindings(viewModel);
 })(); 
